ef2ca20a5c6e8490a70c9fe3c264fddb1caf7eb4
luzuheng <751564347@qq.com>
2018-12-12 09:04:56 +0800

    引入 spring boot && 配置启动 web 服务

--- src/main/java/com/loodeer/App.java
+++ src/main/java/com/loodeer/App.java
@@ -1,13 +1,18 @@
 package com.loodeer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 /**
  * Hello world!
  *
  */
+@EnableAutoConfiguration
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
