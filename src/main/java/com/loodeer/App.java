2590a442220beebea91b9f543351ece6c82c2b65
luzuheng <751564347@qq.com>
2018-12-12 09:14:30 +0800

    web 路径访问 && 修改 server 端口

--- src/main/java/com/loodeer/App.java
+++ src/main/java/com/loodeer/App.java
@@ -2,17 +2,25 @@ package com.loodeer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello world!
  *
  */
 @EnableAutoConfiguration
+@RestController
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         SpringApplication.run(App.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World";
     }
 }
