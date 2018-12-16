e2e8277524eb5e9382505dc7b7f2fe1ced04adb4
luzuheng <751564347@qq.com>
2018-12-16 20:07:47 +0800

    设计商品领域模型、表 && mybatis generator 自动生成 DO DAO Mapper 文件

--- README.md
+++ README.md
@@ -1,22 +1,28 @@
 
 
 
-- 短信验证码获取接口
+- 短信验证码获取接口 `com.loodeer.controller.UserController.getOtp`
 
-- 前端 ajax 调用接口获取验证码
+- 前端 ajax 调用接口获取验证码 `html/getotp.html`
     - jquery 使用
     - CrossOrigin 注解
 
-- 用户注册接口
+- 用户注册接口 `com.loodeer.controller.UserController.register`
     - apache-common-lang 包使用
     - Transactional 事务注解
 
-- 前端用户注册页面
+- 前端用户注册页面 `html/register.html`
     - MD5Encoder 默认只能 encode 16 位的字符串
     - 允许跨域 @CrossOrigin(allowCredentials="true", allowedHeaders = "*")
         - allowedHeaders 允许前端将 token 放入 header 做 session 共享的跨域请求。
         - allowCredentials 授信后，需前端也设置 xhfFields 授信才能实现跨域 session 共享。xhrFields: {withCredentials: true}, 
     - insertSelective 中设置 keyProperty="id" useGeneratedKeys="true" 使得插入完后的 DO 生成自增 id 。
     
- - 用户登陆接口
+ - 用户登陆接口 `com.loodeer.controller.UserController.login`
     - Mapper.xml 文件里 parameterType、jdbcType 分别指定入参和数据库字段类型。
+    
+ - 登陆前端页面 `html/login.html`
+ 
+ - 设计商品领域模型 `com.loodeer.service.model.ItemModel`
+    - 再设计表结构。 item 表、item_stock 表
+    - mybatis generator 自动生成 DO DAO Mapper
---
+++ src/main/java/com/loodeer/service/model/ItemModel.java
@@ -0,0 +1,67 @@
+package com.loodeer.service.model;
+
+public class ItemModel {
+        private Integer id;
+        private String title;
+        private Integer price;
+        private Integer stock;
+        private String description;
+        private Integer sales;
+        private String imgUrl;
+
+        public Integer getId() {
+                return id;
+        }
+
+        public void setId(Integer id) {
+                this.id = id;
+        }
+
+        public String getTitle() {
+                return title;
+        }
+
+        public void setTitle(String title) {
+                this.title = title;
+        }
+
+        public Integer getPrice() {
+                return price;
+        }
+
+        public void setPrice(Integer price) {
+                this.price = price;
+        }
+
+        public Integer getStock() {
+                return stock;
+        }
+
+        public void setStock(Integer stock) {
+                this.stock = stock;
+        }
+
+        public String getDescription() {
+                return description;
+        }
+
+        public void setDescription(String description) {
+                this.description = description;
+        }
+
+        public Integer getSales() {
+                return sales;
+        }
+
+        public void setSales(Integer sales) {
+                this.sales = sales;
+        }
+
+        public String getImgUrl() {
+                return imgUrl;
+        }
+
+        public void setImgUrl(String imgUrl) {
+                this.imgUrl = imgUrl;
+        }
+}
