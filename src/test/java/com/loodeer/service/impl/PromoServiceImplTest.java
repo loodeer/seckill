44ebf944f740dc0712fb3a7c52097424ae54484e
luzuheng <751564347@qq.com>
2018-12-20 01:43:58 +0800

    SpringJUnit4ClassRunner 引入不了，why?

---
+++ src/test/java/com/loodeer/service/impl/PromoServiceImplTest.java
@@ -0,0 +1,22 @@
+package com.loodeer.service.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.annotation.Resource;
+
+/**
+ * @author loodeer
+ * @date 2018-12-20 01:14
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class PromoServiceImplTest {
+
+    @Resource
+    private PromoServiceImpl promoService;
+
+    @Test
+    public void getPromoByItemIdTest() {
+        promoService.getPromoByItemId(10);
+    }
+}
